b901696303c02126232b6533baa10f484de649aa
Maxsuel Lima <maxsuelslima@gmail.com>
2023-09-20 15:35:03 -0300

    Favicon added.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -16,6 +16,16 @@ export default function manifest(): MetadataRoute.Manifest {
         type: 'image/x-icon'
       },
       {
+        src: '/manifest/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png'
+      },
+      {
+        src: '/manifest/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png'
+      },
+      {
         src: '/manifest/apple-touch-icon.png',
         sizes: '180x180',
         type: 'image/png'
