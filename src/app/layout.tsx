167c0f4483080f7966f0aa8801206b862e8ad5e3
Maxsuel Lima <maxsuelslima@gmail.com>
2023-09-20 15:37:11 -0300

    Add favico.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { type ReactElement } from 'react'
 import '@/styles/fonts.css'
 import '@/styles/globals.css'
 import StyledComponentsRegistry from '@/lib/registry'
+import Head from 'next/head'
 
 export const metadata = {
   title: 'ACTITUD-63',
@@ -16,6 +17,9 @@ export default function RootLayout({
 }): ReactElement {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
